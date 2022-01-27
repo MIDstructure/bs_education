bd2854c19329974259b245904727fdaae6d75f11
MIDstructure <midstructure@mail.ru>
2022-01-27 23:31:57 +0600

    Footer + few changes

--- resources/react/modules/Footer.jsx
+++ resources/react/modules/Footer.jsx
@@ -1,7 +1,205 @@
-import React from 'react';
+import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+    return (
+        <div>
+            <footer className="footer">
+                <div className="footer-container">
+                    <div className="footer-row">
+                        <div className="footer-column">
+                            <div className="col-md">
+                                <h2 className="col-md-head">Men</h2>
+                                <ul className="list-unstyled">
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Jeans
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Jumpers
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Leather
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Shorts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Boots
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Watches
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md">
+                                <h2 className="col-md-head">Women</h2>
+                                <ul className="list-unstyled">
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Dresses
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Jumpsuits
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Shirts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            T-shirts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Swearshirts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Jackets
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Coats
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md">
+                                <h2 className="col-md-head">Kids</h2>
+                                <ul className="list-unstyled">
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Shirts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            T-shirts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Polos
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Sweaters
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Sweatshirts
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Jackets
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" className=" md-block">
+                                            Watches
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="footer-column">
+                            <div className="col-md">
+                                <h2 className="col-md-head">Follow Us</h2>
+                                <ul className="footer-social">
+                                    <li className="ftco-animate">
+                                        <a
+                                            href="#"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Twitter"
+                                        >
+                                            <span className="ion-logo">facebook</span>
+                                        </a>
+                                    </li>
+                                    <li className="ftco-animate">
+                                        <a
+                                            href="#"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Facebook"
+                                        >
+                                            <span className="ion-logo">twitter</span>
+                                        </a>
+                                    </li>
+                                    <li className="ftco-animate">
+                                        <a
+                                            href="#"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Instagram"
+                                        >
+                                            <span className="ion-logo">telegram</span>
+                                        </a>
+                                    </li>
+                                    <li className="ftco-animate">
+                                        <a
+                                            href="#"
+                                            data-toggle="tooltip"
+                                            data-placement="top"
+                                            title="Linkedin"
+                                        >
+                                            <span className="ion-logo">whatsapp</span>
+                                        </a>
+                                    </li>
+                                </ul>
+
+                                <h2 className="col-md-head">
+                                    Subscribe Us
+                                </h2>
+                                <input type="text" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="footer-row">
+                        <div className="footer-column">
+                            <p className="copyright">
+                                Copyright &copy; All rights reserved | This template is made with "mid" by
+                                <a href="https://colorlib.com">
+                                    MIDstructure.com
+                                </a>
+                            </p>
+                        </div>
+                        <div className="footer-column">
+                            <p className="list-unstyled">
+                                <a className="ter-priv" href="#">
+                                    Terms &amp; Conditions
+                                </a>
+                                <a className="ter-priv" href="#">
+                                    Privacy
+                                </a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    );
 }
 
 export default Footer;
--- resources/react/routes/Main.jsx
+++ resources/react/routes/Main.jsx
@@ -1,16 +1,8 @@
-import { Button, ButtonGroup, Typography } from '@mui/material';
 import React, { useState } from 'react';
 import { Honey } from '../includes';
-import ThemeStore from '../store/ThemeStore';
-
-import { styled, Box } from '@mui/system';
-import ModalUnstyled from '@mui/base/ModalUnstyled';
-import Modall from "../components/Modall";
 
 
 function Main() {
-
-  const [modal, setModal] = useState(false);
 
   return (
     <Honey/>
